629cccd6c35db4dfc757086662c51e3e58d781b3
Tiara Agustin <132027408+tiaraagustinn@users.noreply.github.com>
2023-11-14 22:43:18 +0700

    Update SMA.java

--- SMA.java
+++ SMA.java
@@ -18,15 +18,17 @@ public class SMA extends School implements Extracurricular
     private String classDept;
     
 
-    /**
-     * Constructor for objects of class SMA
-     */
-    public SMA(int grade, String codeClass, int studentNum)
-    {
+   /**
+   * Constructor for objects of class SMA
+   */
+   public SMA(int grade, String codeClass, int studentNum)
+   {
         super(grade);
         this.codeClass = codeClass;
         maxStudent = studentNum;
-    }
+        students = new ArrayList<Student>();
+        teachers = new ArrayList<Teacher>();
+   }
 
     /**
      * An example of a method - replace this comment with your own
@@ -53,51 +55,70 @@ public class SMA extends School implements Extracurricular
        course.add(sub);
    }
 
-@Override
-public void setGrade() {
-    throw new UnsupportedOperationException("Unimplemented method 'setGrade'");
-}
-
-@Override
-public ArrayList<String> getCourse() {
-    return super.getCourse();
-}
-
-public void addStudent (Student student){
-    if (students.size()==maxStudent) {
-        System.out.println("Class is full");
-    }else{
-        students.add(student);
+    @Override
+    public void setGrade() {
+        throw new UnsupportedOperationException("Unimplemented method 'setGrade'");
     }
-}
 
-public void addTeachers(Teacher teacher) {
-    teachers.add(teacher);
-}
-
-public void setDepartment(String dept) {
-    if(dept.equalsIgnoreCase("bahasa")){
-        classDept = department[0];
-    }else if(dept.equalsIgnoreCase("IPA")){
-        classDept = department[1];
-    }else if(dept.equalsIgnoreCase("bahasa")){
-        classDept = department[3];
-    }else{
-        System.out.println("Tidak ada jurusan yang Anda masukkan di kelas ini");
+    @Override
+    public ArrayList<String> getCourse() {
+        return super.getCourse();
     }
-}
 
-public String getClassDept() {
-    return classDept;
-}
+    public void addStudent (Student student){
+        if (students.size()==maxStudent) {
+            System.out.println("Class is full");
+        }else{
+            students.add(student);
+        }
+    }
 
-@Override
-public String DayOfExc() {
-    return "Pramuka: Sabtu, Rohis: Jumat, OSIS: setiap hari ngerodi";
-}
+    public void addTeachers(Teacher teacher) {
+        teachers.add(teacher);
+    }
 
-@Override
-public String NameOfExc() {
-    return "Pramuka, Rohis, OSIS";
-}
+    public void setDepartment(String dept) {
+        if(dept.equalsIgnoreCase("bahasa")){
+            classDept = department[0];
+        }else if(dept.equalsIgnoreCase("IPA")){
+            classDept = department[1];
+        }else if(dept.equalsIgnoreCase("bahasa")){
+            classDept = department[3];
+        }else{
+            System.out.println("Tidak ada jurusan yang Anda masukkan di kelas ini");
+        }
+    }
+
+    public String getClassDept() {
+        return classDept;
+    }
+
+    @Override
+    public String DayOfExc() {
+        return "Pramuka: Sabtu, Rohis: Jumat, OSIS: setiap hari ngerodi";
+    }
+
+    @Override
+    public String NameOfExc() {
+        return "Pramuka, Rohis, OSIS";
+    }
+    
+    public void printList()
+    {
+        System.out.println("\t\t+-----------------------+");
+        System.out.println("\t\t|\tSiswa SMA\t|");
+        System.out.println("\t\t+-----------------------+");
+        System.out.println("Room: " + codeClass + "\n");
+        System.out.println("Class list:");
+        for(Student student : students) {
+            System.out.print("- ");
+            student.print();
+        }
+        System.out.println("\nTeacher: ");
+        for(Teacher teacher : teachers) {
+            System.out.print("- ");
+            teacher.print();
+        }
+        System.out.println("\nNumber of students: " + students.size());
+    }
 }
